86de8e121491e05425e776e83f1c6b1fcabfab16
Jacob Willis <jbwillis272@gmail.com>
2021-09-02 08:20:07 -0600

    Plot frame rate

--- tools/FPS.py
+++ tools/FPS.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import argparse
 import time
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='Capture an image from the given camera number')
 
@@ -12,7 +13,9 @@ parser.add_argument('cam', help='OpenCV camera number (0 indexed)', type=int)
 
 args = parser.parse_args()
 
+# read once to get the camera started
 cap = cv2.VideoCapture(args.cam)
+ret, frame = cap.read()
 
 t_vec = np.zeros(100)
 
@@ -28,6 +31,8 @@ print("Average FPS: ", 1./np.mean(t_vec))
 print("Max FPS: ", 1./np.min(t_vec))
 print("Min FPS: ", 1./np.max(t_vec))
         
+plt.hist(1./t_vec)
+plt.show()
 cap.release()
 cv2.destroyAllWindows()
 
